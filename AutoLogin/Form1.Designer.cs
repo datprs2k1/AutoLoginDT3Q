ee532dce8ab88a52e46d8802865270453c5c8e76
datprs <datprs@gmail.com>
2023-05-07 08:50:36 +0700

    Add project files.

---
+++ AutoLogin/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace AutoLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList1 = new System.Windows.Forms.Button();
+            this.dtgData = new System.Windows.Forms.DataGridView();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnList1
+            // 
+            this.btnList1.Location = new System.Drawing.Point(45, 250);
+            this.btnList1.Name = "btnList1";
+            this.btnList1.Size = new System.Drawing.Size(115, 76);
+            this.btnList1.TabIndex = 0;
+            this.btnList1.Text = "List 1";
+            this.btnList1.UseVisualStyleBackColor = true;
+            this.btnList1.Click += new System.EventHandler(this.btnList1_Click);
+            // 
+            // dtgData
+            // 
+            this.dtgData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserName});
+            this.dtgData.Location = new System.Drawing.Point(45, 37);
+            this.dtgData.Name = "dtgData";
+            this.dtgData.RowHeadersWidth = 51;
+            this.dtgData.RowTemplate.Height = 24;
+            this.dtgData.Size = new System.Drawing.Size(297, 150);
+            this.dtgData.TabIndex = 1;
+            // 
+            // UserName
+            // 
+            this.UserName.DataPropertyName = "UserName";
+            this.UserName.HeaderText = "Tài khoản";
+            this.UserName.MinimumWidth = 6;
+            this.UserName.Name = "UserName";
+            this.UserName.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 76);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "List 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 76);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "List 3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(646, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 76);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "List 4";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(159, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 63);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Lỗi hệ thống";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name1});
+            this.dataGridView1.Location = new System.Drawing.Point(456, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(305, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(456, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 63);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Thiết bị";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Name1
+            // 
+            this.Name1.DataPropertyName = "Name";
+            this.Name1.HeaderText = "Tên";
+            this.Name1.MinimumWidth = 6;
+            this.Name1.Name = "Name1";
+            this.Name1.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtgData);
+            this.Controls.Add(this.btnList1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnList1;
+        private System.Windows.Forms.DataGridView dtgData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+    }
+}
+
---
+++ AutoLogin/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoLogin
+{
+    public partial class Form1 : Form
+    {
+        public List<Account> list = new List<Account>();
+        public List<string> listDevice = new List<string>();
+        public Bitmap LoiHeThong;
+        public Form1()
+        {
+            InitializeComponent();
+
+            FileStream file = new FileStream("Data/Account.txt", FileMode.Open, FileAccess.Read);
+
+            StreamReader sr = new StreamReader(file);
+
+            list.Clear();
+
+            string str;
+
+            while ((str = sr.ReadLine()) != null)
+            {
+                string[] Account = str.Split('|');
+
+                Account a = new Account();
+
+                a.UserName = Account[0];
+                a.Password = Account[1];
+
+                list.Add(a);
+            }
+
+            sr.Close();
+            file.Close();
+
+            dtgData.AutoGenerateColumns = false;
+            dtgData.DataSource = list;
+
+            listDevice = KAutoHelper.ADBHelper.GetDevices();
+
+            LoiHeThong = (Bitmap)Bitmap.FromFile("Data/loihethong.png");
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = listDevice.Select(x => new
+            {
+                Name = x
+            }).ToList();
+        }
+
+        private void btnList1_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+
+            void Auto()
+            {
+
+                int i = -1;
+                if (listDevice.Count > 0)
+                {
+                    listDevice.ForEach((x) =>
+                    {
+
+                        Task a = new Task(() =>
+                        {
+                            i++;
+                            KAutoHelper.ADBHelper.TapByPercent(x, 51.0, 44.0);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].UserName.ToString());
+                            KAutoHelper.ADBHelper.TapByPercent(x, 57.9, 52.3);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].Password.ToString());
+                        });
+                        a.RunSynchronously();
+
+                    });
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+
+            void Auto()
+            {
+                int i = 8;
+                if (listDevice.Count > 0)
+                {
+                    listDevice.ForEach((x) =>
+                    {
+                        Task a = new Task(() =>
+                        {
+                            KAutoHelper.ADBHelper.TapByPercent(x, 51.0, 44.0);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].UserName.ToString());
+                            KAutoHelper.ADBHelper.TapByPercent(x, 57.9, 52.3);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].Password.ToString());
+                        });
+                        a.RunSynchronously();
+                        i++;
+
+
+                    });
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+
+            void Auto()
+            {
+                int i = 16;
+                if (listDevice.Count > 0)
+                {
+                    listDevice.ForEach((x) =>
+                    {
+                        Task a = new Task(() =>
+                        {
+                            KAutoHelper.ADBHelper.TapByPercent(x, 51.0, 44.0);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].UserName.ToString());
+                            KAutoHelper.ADBHelper.TapByPercent(x, 57.9, 52.3);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].Password.ToString());
+                        });
+                        a.RunSynchronously();
+                        i++;
+
+                    });
+                }
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+
+            void Auto()
+            {
+                int i = 24;
+                if (listDevice.Count > 0)
+                {
+                    listDevice.ForEach((x) =>
+                    {
+                        Task a = new Task(() =>
+                        {
+                            KAutoHelper.ADBHelper.TapByPercent(x, 51.0, 44.0);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].UserName.ToString());
+                            KAutoHelper.ADBHelper.TapByPercent(x, 57.9, 52.3);
+                            KAutoHelper.ADBHelper.InputText(x, list[i].Password.ToString());
+                        });
+                        a.RunSynchronously();
+                        i++;
+
+                    });
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+
+            void Auto()
+            {
+                int i = 16;
+                if (listDevice.Count > 0)
+                {
+                    listDevice.ForEach((x) =>
+                    {
+                        Task a = new Task(() =>
+                        {
+                            KAutoHelper.ADBHelper.TapByPercent(x, 65.9, 67.2);
+                            KAutoHelper.ADBHelper.Delay(1);
+                            KAutoHelper.ADBHelper.TapByPercent(x, 52.7, 60.1);
+                            KAutoHelper.ADBHelper.Delay(1);
+                            KAutoHelper.ADBHelper.TapByPercent(x, 58.1, 84.2);
+                            KAutoHelper.ADBHelper.Delay(1);
+                            KAutoHelper.ADBHelper.TapByPercent(x, 47.0, 43.4);
+                            KAutoHelper.ADBHelper.Delay(1);
+                            KAutoHelper.ADBHelper.InputText(x, list[0].Password.ToString());
+                        });
+                        a.Start();
+                        i++;
+
+                    });
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listDevice = KAutoHelper.ADBHelper.GetDevices();
+            dataGridView1.DataSource = listDevice.Select(x => new
+            {
+                Name = x
+            }).ToList();
+
+            dataGridView1.Refresh();
+        }
+    }
+}
